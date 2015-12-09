3b53065ab0da4ff466425f088f70c49dc844ce6a
Julia Will <julia@hoverstate.com>
2015-12-09 21:21:18 +0100

    Initial Commit

---
+++ FamilyPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FamilyPlanner
+//
+//  Created by Julia Will on 09.12.15.
+//  Copyright © 2015 Julia Will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

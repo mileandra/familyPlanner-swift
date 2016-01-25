a7a7e3b3430e7a36ffbf6c1f4d74f5c2d231c05d
Julia Will <julia@hoverstate.com>
2016-01-25 20:23:26 +0100

    Adding some additional layout to messages

--- FamilyPlanner/MessageCollectionViewCell.swift
+++ FamilyPlanner/MessageCollectionViewCell.swift
@@ -11,8 +11,22 @@ import UIKit
 class MessageCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var messageText: UILabel!
+    var own:Bool = true
     
     override func drawRect(rect: CGRect) {
-        super.drawRect(rect)
+        messageText.sizeToFit()
+        let frame = messageText.frame
+        let padding:CGFloat = 5.0
+        
+        //TODO: switch sides and color by user
+        if own == true {
+            //// Color Declarations
+            let color = UIColor(red: 0.926, green: 0.971, blue: 0.965, alpha: 1.000)
+            
+            //// Rectangle Drawing
+            let rectanglePath = UIBezierPath(roundedRect: CGRectMake(frame.minX-padding, frame.minY-padding, frame.maxX+padding, frame.maxY+padding), cornerRadius: 5)
+            color.setFill()
+            rectanglePath.fill()
+        }
     }
 }
